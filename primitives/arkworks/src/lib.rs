64678db0b6ae1cdf07a488ed5b24ac7fa0dcac51
Marcin <marcin.gorny.94@protonmail.com>
2022-10-10 15:56:42 +0200

    Add an arkworks primitive

---
+++ primitives/arkworks/src/lib.rs
@@ -0,0 +1,39 @@
+// This file is part of Substrate.
+
+// Copyright (C) 2017-2022 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Hashing Functions.
+
+#![warn(missing_docs)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ark_bls12_381::{Bls12_381, G1Affine, G2Affine};
+use ark_ec::{
+	bls12::{G1Prepared, G2Prepared},
+	pairing::{self, *},
+	AffineRepr, CurveGroup, Group,
+};
+use ark_ff::{Field, PrimeField};
+use ark_serialize::CanonicalSerialize;
+use sp_std::vec::Vec;
+
+pub fn pairing(a: &[u8], b: &[u8]) -> Vec<u8> {
+	let res = Bls12_381::pairing(&G1Affine::generator(), &G2Affine::generator());
+	// serialize the result
+	let mut res_bytes = [0u8; 96];
+	res.0.serialize_compressed(&mut res_bytes[..]).unwrap();
+	res_bytes.to_vec()
+}
