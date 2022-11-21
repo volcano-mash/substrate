46b3c67f24e399a5f0e26a54dac741345b4dd998
achimcc <achim.schneider@posteo.de>
2022-11-21 13:03:06 +0000

    Add secon generic param to model

--- primitives/arkworks/bls12/src/lib.rs
+++ primitives/arkworks/bls12/src/lib.rs
@@ -8,7 +8,6 @@ use ark_ff::fields::fp12_2over3over2::Fp12;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress};
 use ark_std::{io::Cursor, marker::PhantomData, vec, vec::Vec};
 use derivative::Derivative;
-use sp_io::crypto::bls12_381_multi_miller_loop;
 
 #[cfg(feature = "parallel")]
 use rayon::prelude::*;
@@ -21,16 +20,19 @@ pub mod g2;
 
 pub use self::g2::{G2Affine, G2Prepared, G2Projective};
 
-#[derive(Derivative)]
-#[derivative(Copy, Clone, PartialEq, Eq, Debug, Hash)]
-pub struct Bls12<P: Bls12Parameters>(PhantomData<fn() -> P>);
-
-pub trait HostFunctions {
+pub trait HostFunctions : 'static {
 	fn multi_miller_loop(a_vec: Vec<Vec<u8>>, b_vec: Vec<Vec<u8>>) -> Vec<u8>;
-    fn final_exponentiation(f12: &[u8]) -> Vec<u8> ;
+	fn final_exponentiation(f12: &[u8]) -> Vec<u8>;
 }
 
-impl<P: Bls12Parameters> Pairing for Bls12<P> {
+#[derive(Derivative)]
+#[derivative(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+pub struct Bls12<P: Bls12Parameters, Q: HostFunctions> {
+	phantom1: PhantomData<fn() -> P>,
+	phantom2: PhantomData<fn() -> Q>
+}
+
+impl<P: Bls12Parameters, Q: HostFunctions> Pairing for Bls12<P, Q> {
 	type BaseField = <P::G1Parameters as CurveConfig>::BaseField;
 	type ScalarField = <P::G1Parameters as CurveConfig>::ScalarField;
 	type G1 = G1Projective<P>;
@@ -66,7 +68,7 @@ impl<P: Bls12Parameters> Pairing for Bls12<P> {
 			})
 			.collect();
 
-		let res = bls12_381_multi_miller_loop(a_vec, b_vec);
+		let res = Q::multi_miller_loop(a_vec, b_vec);
 		let cursor = Cursor::new(&res[..]);
 		let f: Self::TargetField =
 			Fp12::deserialize_with_mode(cursor, Compress::Yes, ark_serialize::Validate::No)
@@ -80,7 +82,7 @@ impl<P: Bls12Parameters> Pairing for Bls12<P> {
 		let mut cursor = Cursor::new(&mut out[..]);
 		f.0.serialize_with_mode(&mut cursor, Compress::Yes).unwrap();
 
-		let res = sp_io::crypto::bls12_381_final_exponentiation(&out[..]);
+		let res = Q::final_exponentiation(&out[..]);
 
 		let cursor = Cursor::new(&res[..]);
 		let r: Self::TargetField =
